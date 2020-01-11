318271f35b2c915aa55de824a16dc1014221e499
efriden <44654018+efriden@users.noreply.github.com>
2020-01-11 23:19:50 +0100

    Update visitors.php

--- visitors.php
+++ visitors.php
@@ -1,13 +1,50 @@
 <?php
 
 /** 
- * Det här programmet räknar antalet
- * besökare på sidan.
+ * Det här programmet håller koll på och skriver ut hur
+ * många gånger hemsidan har besökts. Besökarens datum,
+ * tid, IP-adress och webbläsare loggas i en separat fil.
  */
 
 $counter_name = 'counter.txt';
+$visitors_name = 'visitors.txt';
+$date = date('Y-m-d, H:i:s');
+$visitorIP = $_SERVER['REMOTE_ADDR'];
 
-// Om filen inte existerar skapa ny med startvärde 0
+// Webbklientens webbläsare
+if (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== false) {
+	$visitorBrowser = 'Internet explorer';
+} elseif (strpos($_SERVER['HTTP_USER_AGENT'], 'Trident') !== false) {
+	$visitorBrowser = 'Internet explorer';
+} elseif (strpos($_SERVER['HTTP_USER_AGENT'], 'Edge') !== false) {
+	$visitorBrowser = 'Microsoft Edge';
+} elseif (strpos($_SERVER['HTTP_USER_AGENT'], 'Firefox') !== false) {
+	$visitorBrowser = 'Mozilla Firefox';
+} elseif (strpos($_SERVER['HTTP_USER_AGENT'], 'Chrome') !== false) {
+	$visitorBrowser = 'Google Chrome';
+} elseif (strpos($_SERVER['HTTP_USER_AGENT'], 'Opera Mini') !== false) {
+	$visitorBrowser = 'Opera Mini';
+} elseif (strpos($_SERVER['HTTP_USER_AGENT'], 'Opera') !== false) {
+	$visitorBrowser = 'Opera';
+} elseif (strpos($_SERVER['HTTP_USER_AGENT'], 'Safari') !== false) {
+	$visitorBrowser = 'Safari';
+} else {
+	$visitorBrowser = 'Other';
+}
+
+$visitorData = $date . ', ' . $visitorBrowser . ', ' . $visitorIP;
+
+// Logga besökare på fil
+$f = fopen($visitors_name, 'a+');
+	if (flock($f, LOCK_EX)) {
+		fwrite($f, $visitorData . "\n");
+		flock($f, LOCK_UN);
+	}
+	fclose($f);
+
+//--------Räkna antalet besökare------------------
+
+// Om filen inte existerar: skapa med startvärde 0
 if (!file_exists($counter_name)) {
 	$f = fopen($counter_name, 'w');
 	if (flock($f, LOCK_EX)) {
@@ -36,10 +73,10 @@ if (flock($f, LOCK_EX)) {
 }
 fclose($f);
 
+// Byt ut värdet i html-sidan
 $html = file_get_contents("home.html");
 $html = str_replace('---$hits---', $counterVal, $html);
 
 echo $html;
-
 
 ?>
